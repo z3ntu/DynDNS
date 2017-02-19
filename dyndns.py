231850c89cce1c0328f949641c8920bd21befb44
Luca Weiss <luca@z3ntu.xyz>
2017-02-19 13:28:59 +0100

    Fix dns with more than 20 entries (multiple pages) and fix exception when the auth_keys file is not found.

--- dyndns.py
+++ dyndns.py
@@ -15,6 +15,7 @@ app = flask.Flask(__name__)
 
 os.makedirs("letsencrypt-webroot/.well-known/acme-challenge", exist_ok=True)
 
+
 @app.route('/')
 def root():
     return "DynDNS server by z3ntu."
@@ -27,11 +28,14 @@ def update_record():
     ipv4 = False
     ipv6 = False
 
+    # Check if there's a request body
     if not flask.request.json:
         return "No body."
+    # Check if all parameters are given
     if not flask.request.json.get('auth_key') or not flask.request.json.get('host') or not flask.request.json.get(
             'ip'):
         return "Missing parameter."
+    # Check if the given IP is IPv4 or IPv6
     try:
         socket.inet_pton(socket.AF_INET6, flask.request.json.get('ip'))
         ipv6 = True
@@ -41,13 +45,36 @@ def update_record():
             ipv4 = True
         except OSError:
             return "Invalid IPv4/IPv6."
-    if not is_valid_auth_key(flask.request.json.get('auth_key')):
-        return "Invalid key."
-    records_req = requests.get(do_api_base + "/v2/domains/" + config_server.do_domain + "/records",
-                               headers=request_header)
-    if records_req.status_code != 200:
-        return "Internal error: " + str(records_req.status_code) + " - " + records_req.reason
-    records = json.loads(records_req.text)
+    try:
+        if not is_valid_auth_key(flask.request.json.get('auth_key')):
+            return "Invalid key."
+    except FileNotFoundError:
+        return "'auth_keys' file not found. Please fix your server."
+    # set the initial request url
+    do_request_url = do_api_base + "/v2/domains/" + config_server.do_domain + "/records"
+    records = {}
+    # loop for multiple pages
+    while True:
+        records_req = requests.get(do_request_url,
+                                   headers=request_header)
+        if records_req.status_code != 200:
+            return "Internal error: " + str(records_req.status_code) + " - " + records_req.reason
+        # 'internal' records variable
+        records_int = json.loads(records_req.text)
+        if not records_int.get('links'):  # if there are multiple pages
+            # Just set the variable without extending it
+            records = records_int
+            break
+        # get the request url for the next page
+        do_request_url = records_int.get('links').get('pages').get('next')
+        # if 'records' is empty, set it, otherwise extend the 'domain_records' variable
+        if not records:
+            records = records_int
+        else:
+            records.get('domain_records').extend(records_int.get('domain_records'))
+        # if there's no 'next' url, we are at the last page -> break out of the loop
+        if not do_request_url:
+            break
     record_id = None
     for record in records.get('domain_records'):
         if record.get('name') == flask.request.json.get('host'):
@@ -69,8 +96,7 @@ def update_record():
             record_id = record.get('id')
             break
     if not record_id:
-        return "Host not found."  # Create host???
-
+        return "Host not found."  # Should we create the host here?
     if action is "update_ipv6" or action is "update_ipv4":
         # Update existing record
         data = {"data": flask.request.json.get('ip')}
@@ -95,6 +121,7 @@ def update_record():
     return "Success."
 
 
+# Handle LetsEncrypt/Certbot requests
 @app.route('/.well-known/acme-challenge/<path:filename>')
 def letsencrypt_handler(filename):
     return flask.send_from_directory("letsencrypt-webroot/.well-known/acme-challenge", filename)
